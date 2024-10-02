bcfb48f202b15b9e6720fd9b6bff608d211bca4a
Kynam L <115427320+Noto501@users.noreply.github.com>
2024-10-02 15:24:05 -0700

    fix: renamed CatzAutoRoutine to CatzAutonomous

--- Crecendo-2024-Refurbished/src/main/java/frc/robot/BuildConstants.java
+++ Crecendo-2024-Refurbished/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crecendo-2024-Refurbished";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 56;
-  public static final String GIT_SHA = "608aea8f6964121686f6883ad5dca98391473c36";
-  public static final String GIT_DATE = "2024-09-28 19:12:22 EDT";
-  public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2024-09-29 11:03:47 EDT";
-  public static final long BUILD_UNIX_TIME = 1727622227360L;
+  public static final int GIT_REVISION = 63;
+  public static final String GIT_SHA = "803e977517409c214b2843dda2040de251bacff8";
+  public static final String GIT_DATE = "2024-10-02 17:53:44 EDT";
+  public static final String GIT_BRANCH = "Trajectory-Updates";
+  public static final String BUILD_DATE = "2024-10-02 18:22:52 EDT";
+  public static final long BUILD_UNIX_TIME = 1727907772814L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
